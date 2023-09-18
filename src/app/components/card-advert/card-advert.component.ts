715ca0f2856f5a8ff76573f6fb93a7f84d3d83aa
mattsol <mattsolnew@gmail.com>
2023-09-18 21:33:27 +0300

    edit card content

--- src/app/components/card-advert/card-advert.component.ts
+++ src/app/components/card-advert/card-advert.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-advert.component.scss']
 })
 export class CardAdvertComponent {
+	items = [
+		{
+			name: 'Гитара Fender',
+			price: 20000,
+			location: 'Москва, Ленинский проспект',
+			time: 'Сегодня 14:12'
+		},
 
+		{
+			name: 'MacbookPro 14',
+			price: 30000,
+			location: 'Севастополь, Ленинский проспект',
+			time: 'Сегодня 11:12'
+		},
+
+		{
+			name: 'Посудомойка',
+			price: 40000,
+			location: 'Краснодар, Ленинский проспект',
+			time: 'Сегодня 10:12'
+		},
+	]
 }
